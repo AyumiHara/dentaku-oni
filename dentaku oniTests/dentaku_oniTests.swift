07fe52551bc952dc2a8fc2e478d497182f5b969b
AyumiHara <mapledog0425@gmail.com>
2018-04-13 02:46:05 +0900

    Initial Commit

---
+++ dentaku oniTests/dentaku_oniTests.swift
@@ -0,0 +1,36 @@
+//
+//  dentaku_oniTests.swift
+//  dentaku oniTests
+//
+//  Created by 原 あゆみ on 2018/04/09.
+//  Copyright © 2018年 原あゆみ. All rights reserved.
+//
+
+import XCTest
+@testable import dentaku_oni
+
+class dentaku_oniTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
